e29c11297cd1cb3ad92f0d22d69e353b65c2277a
koberman758 <koberman758@gmail.com>
2023-03-28 10:59:07 +0300

    Homework

--- src/test/java/wrappers/DropDown.java
+++ src/test/java/wrappers/DropDown.java
@@ -7,24 +7,6 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class DropDown {
-    String label;
-    WebDriver driver;
-    String baseLocator = "//span[contains(text(), '%s')]/ancestor::div[contains(@class, 'uiInputSelect')]//a";
-
-    public DropDown(WebDriver driver, String label) {
-        this.driver = driver;
-        this.label = label;
-    }
-
-    public void select(String text) {
-        driver.findElement(By.xpath(String.format(baseLocator, label))).click();
-        List<WebElement> options = driver.findElements(By.xpath("//div[@class='select-options']//li"));
-
-        for (WebElement option : options) {
-            if (option.getText().equals(text)) {
-                option.click();
-                break;
-            }
-        }
-    }
+   
+    
 }
