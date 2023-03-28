96e88dfa4951d23cb399eba12fa93dc935cadba8
koberman758 <koberman758@gmail.com>
2023-03-28 11:23:53 +0300

    Homework

--- src/test/java/wrappers/DropDown.java
+++ src/test/java/wrappers/DropDown.java
@@ -1,10 +1,5 @@
 package wrappers;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
 
 public class DropDown {
    
